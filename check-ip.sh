23c5af895dd1822728332a618357f6723d890320
Saugat Nepal <programmersaugat@gmail.com>
2023-01-30 12:33:30 +0545

    Update check-ip.sh

--- check-ip.sh
+++ check-ip.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-network="192.168.12"
-for i in {1..255}; do
+read -p "Enter the network IP address (e.g. 192.168.12): " network
+read -p "Enter the range of IP addresses to scan (e.g. 1-255): " range
+
+IFS='-' read -a range_array <<< "$range"
+start=${range_array[0]}
+end=${range_array[1]}
+
+for i in $(seq $start $end); do
     ping -c 1 $network.$i > /dev/null
     if [ $? -eq 0 ]; then
         echo "$network.$i is in use"
